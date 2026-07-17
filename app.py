4f305e7e352795c9a092bb3bae0c9f8dd04dbe1d
kunal1415 <kunalwalunj2006@gmail.com>
2026-07-17 20:31:03 +0530

    add PDF upload endpoint

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
+import os
+import shutil
 
-app = FastAPI()
+app = FastAPI(
+    title="PDF RAG API",
+    version="1.0.0"
+)
+
+UPLOAD_FOLDER = "uploads"
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 
 @app.get("/")
 def home():
     return {
-        "message": "Welcome to the PDF RAG API!"
+        "message": "Welcome to the PDF RAG API"
+    }
+
+
+@app.post("/upload")
+async def upload_pdf(file: UploadFile = File(...)):
+    # Validate PDF
+    if file.content_type != "application/pdf":
+        raise HTTPException(
+            status_code=400,
+            detail="Only PDF files are allowed."
+        )
+
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "message": "PDF uploaded successfully",
+        "filename": file.filename
     }
